fe0fd9a651d23a61fa5e7ccba858024e78784131
Nicolas DUMINIL <nicolas.duminil@simplex-software.fr>
2024-11-01 17:37:18 +0100

    Have fixed inheritance issue with base and extended MP REST client interfaces

--- async-services/async-jaxrs20/src/main/java/fr/simplex_software/fifty_shades_of_rest/async_services/jaxrs20/NtpResourceClient.java
+++ async-services/async-jaxrs20/src/main/java/fr/simplex_software/fifty_shades_of_rest/async_services/jaxrs20/NtpResourceClient.java
@@ -2,18 +2,10 @@ package fr.simplex_software.fifty_shades_of_rest.async_services.jaxrs20;
 
 import fr.simplex_software.fifty_shades_of_rest.base.*;
 import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.*;
 import org.eclipse.microprofile.rest.client.inject.*;
 
 @RegisterRestClient(configKey = "base_uri")
 @Path("ntp")
 public interface NtpResourceClient extends BaseMpClient
 {
-  @GET
-  @Produces(MediaType.TEXT_PLAIN)
-  String getCurrentDateAndTimeAtDefaultZone();
-  @GET
-  @Path("{zoneId}")
-  @Produces(MediaType.TEXT_PLAIN)
-  String getCurrentDateAndTimeAtZone(@PathParam("zoneId") String zoneId);
 }
--- classic/src/main/java/fr/simplex_software/fifty_shades_of_rest/classic/CurrentTimeResourceClient.java
+++ classic/src/main/java/fr/simplex_software/fifty_shades_of_rest/classic/CurrentTimeResourceClient.java
@@ -2,18 +2,11 @@ package fr.simplex_software.fifty_shades_of_rest.classic;
 
 import fr.simplex_software.fifty_shades_of_rest.base.*;
 import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.*;
 import org.eclipse.microprofile.rest.client.inject.*;
 
 @RegisterRestClient(configKey = "base_uri")
 @Path("time")
 public interface CurrentTimeResourceClient extends BaseMpClient
 {
-  @GET
-  @Produces(MediaType.TEXT_PLAIN)
-  String getCurrentDateAndTimeAtDefaultZone();
-  @GET
-  @Path("{zoneId}")
-  @Produces(MediaType.TEXT_PLAIN)
-  String getCurrentDateAndTimeAtZone(@PathParam("zoneId") String zoneId);
+
 }
